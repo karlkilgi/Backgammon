0c8e6b46d50a5599d18d2eb6e6f3ad30168a4351
Karl Kilgi <kilgi.karl@gmail.com>
2012-10-09 15:29:34 +0300

    Updated game logic

--- src/ee/ut/cs/sysmodel/Player.java
+++ src/ee/ut/cs/sysmodel/Player.java
@@ -8,21 +8,29 @@ import java.awt.Color;
  * Time: 9:34 PM
  */
 public enum Player {
-    PLAYER1 (Color.RED), PLAYER2(Color.WHITE), NONE;
-    
+    PLAYER1(Color.RED, 0), PLAYER2(Color.WHITE, 25), NONE;
+
     private final Color color;
-    
+
+    public int getHomePoint() {
+        return homePoint;
+    }
+
+    private final int homePoint;
+
     Player() {
-    	this.color = null;
+        this.color = null;
+        this.homePoint = 0;
     }
-    
-    Player(Color color) {
-    	this.color = color;
+
+    Player(Color color, int homePoint) {
+        this.color = color;
+        this.homePoint = homePoint;
     }
-    
+
     public Color getColor() {
-    	return color;
+        return color;
     }
-    
-    
+
+
 }
