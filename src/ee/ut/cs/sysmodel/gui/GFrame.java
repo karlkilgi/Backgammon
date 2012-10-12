b380fcc22f99d78e821bee194c735f25039a9ef2
Siim Viiklaid <siimviik@gmail.com>
2012-10-12 20:31:08 +0300

    GFrame

--- src/ee/ut/cs/sysmodel/gui/GFrame.java
+++ src/ee/ut/cs/sysmodel/gui/GFrame.java
@@ -31,9 +31,9 @@ public class GFrame {
 
 	private GHome player1Home;
 	private GHome player2Home;
-	
+
 	private JFrame frame;
-	
+
 	private GInfo infoPanel;
 
 	public GFrame(Game game) {
@@ -41,37 +41,38 @@ public class GFrame {
 		createFrame();
 	}
 
-    // Is starting position of a move selected
-    public boolean isFromSelected() {
-        return moveFrom != null;
-    }
+	// Is starting position of a move selected
+	public boolean isFromSelected() {
+		return moveFrom != null;
+	}
 
-    // Repaint the whole frame
-    public void refresh() {
-        for (GPoint gPoint : gPoints) {
-            gPoint.refresh();
-        }
-        player1Bar.refresh();
-        player2Bar.refresh();
+	// Repaint the whole frame
+	public void refresh() {
+		for (GPoint gPoint : gPoints) {
+			gPoint.refresh();
+		}
+		player1Bar.refresh();
+		player2Bar.refresh();
 
-        player1Home.refresh();
-        player2Home.refresh();
-        
-        infoPanel.refresh();
-    }
+		player1Home.refresh();
+		player2Home.refresh();
+
+		infoPanel.refresh();
+	}
 
 	// EVENT HANDLERS start here
-	
+
 	// Handles events from points, bars and homes
 	public void onWidgetClick(GWidget widget) {
-	    // If starting position of a move has not been set yet
-	    // then register the widget as one
+		// If starting position of a move has not been set yet
+		// then register the widget as one
 		if (!isFromSelected()) {
 			moveFrom = widget;
 			return;
 		}
 
-		// If widget's starting position is the same as registered starting position
+		// If widget's starting position is the same as registered starting
+		// position
 		// then unregister the starting position (unselect the widget)
 		if (isFromSelected() && moveFrom.getPosition() == widget.getPosition()) {
 			moveFrom.refresh();
@@ -88,142 +89,125 @@ public class GFrame {
 			moveFrom = null;
 		}
 	}
-	
-    public void onDoubleBet() {
-      game.increaseBet();
-      infoPanel.refresh();
-    }
-    
-    public void onNewgame() {
-      String message = "Are you sure you want to cancel the game and start a new one?";
-      Object[] options = {"yea wadeva", "naw bro"};
-      int answer = JOptionPane.showOptionDialog(frame,
-          message,
-          "Start a new game",
-          JOptionPane.OK_CANCEL_OPTION,
-          JOptionPane.QUESTION_MESSAGE,
-          null,
-          options,
-          options[0]);
-      if (answer == 0) {
-        // TODO game.startNewGame();
-      }
-    }
-    
-    // EVENT HANDLERS end here
-	
+
+	public void onDoubleBet() {
+		game.increaseBet();
+		infoPanel.refresh();
+	}
+
+	public void onNewgame() {
+		String message = "Are you sure you want to cancel the game and start a new one?";
+		Object[] options = { "yea wadeva", "naw bro" };
+		int answer = JOptionPane.showOptionDialog(frame, message,
+				"Start a new game", JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		if (answer == 0) {
+			game.startNewGame();
+		}
+	}
+
+	// EVENT HANDLERS end here
+
 	// POP UPS start here
-	
-    public void showStartingPlayerPopup(List<Integer> throwresults) {
-      String message = Player.PLAYER1.getName() + " threw " + throwresults.get(0) + "\n";
-      message += Player.PLAYER2.getName() + " threw " + throwresults.get(1) + "\n\n";
-      message += game.getActivePlayer().getName() + " starts!";
-      showMessagePopup(message, "OK", JOptionPane.INFORMATION_MESSAGE);
-    }
-    
+
+	public void showStartingPlayerPopup(List<Integer> throwresults) {
+		String message = Player.PLAYER1.getName() + " threw "
+				+ throwresults.get(0) + "\n";
+		message += Player.PLAYER2.getName() + " threw " + throwresults.get(1)
+				+ "\n\n";
+		message += game.getActivePlayer().getName() + " starts!";
+		showMessagePopup(message, "OK", JOptionPane.INFORMATION_MESSAGE);
+	}
+
 	public void showChangePlayersPopup() {
 		String message = game.getInActivePlayer().getName()
 				+ ", your turn is over. " + game.getActivePlayer().getName()
 				+ ", it's your move";
-		
-	    Object[] options = {"Throw", "Double bets and throw"};
-	      int answer = JOptionPane.showOptionDialog(frame,
-	          message,
-	          "Doubling the bet",
-	          JOptionPane.DEFAULT_OPTION,
-	          JOptionPane.QUESTION_MESSAGE,
-	          null,
-	          options,
-	          options[0]);
-	      if (answer == 0 || showAcceptDoublingPopup()) {
-	        infoPanel.refresh();
-	        game.onDiceThrow();
-	      }
+
+		Object[] options = { "Throw", "Double bets and throw" };
+		int answer = JOptionPane.showOptionDialog(frame, message,
+				"Doubling the bet", JOptionPane.DEFAULT_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		if (answer == 0 || showAcceptDoublingPopup()) {
+			infoPanel.refresh();
+			game.onDiceThrow();
+		}
 	}
-	
+
 	public void showBeginningPopup() {
 		String message = "Game started. Throw dice to choose starting player";
 		showMessagePopup(message, "Throw", JOptionPane.INFORMATION_MESSAGE);
 	}
-	
+
 	public void showDiceResultsPopup(List<Integer> results) {
-		String message = game.getActivePlayer().getName() + " threw " + getIntegerListAsString(results);
+		String message = game.getActivePlayer().getName() + " threw "
+				+ getIntegerListAsString(results);
 		showMessagePopup(message, "OK", JOptionPane.INFORMATION_MESSAGE);
 	}
-	
+
 	public void showIllegalMovePopUp() {
 		String message = "Illegal move!";
 		showMessagePopup(message, "OK", JOptionPane.WARNING_MESSAGE);
 	}
-	
-	private boolean showAcceptDoublingPopup() {
-	  String message = game.getInActivePlayer().getName() + ", do you accept doubling the bet?";
-	  Object[] options = {"Yes", "No, I'll resign"};
-      int answer = JOptionPane.showOptionDialog(frame,
-          message,
-          "Doubling the bet",
-          JOptionPane.YES_NO_OPTION,
-          JOptionPane.QUESTION_MESSAGE,
-          null,
-          options,
-          options[0]);
-      if (answer == 0) {
-        game.increaseBet();
-        infoPanel.refresh();
-        return true;
-      } else {
-        game.onWin(game.getActivePlayer());
-        return false;
-      }
-	}
-	
-	private void showMessagePopup(String message, String buttonText, int messageType) {
-	  Object[] options = {buttonText};
-	     JOptionPane.showOptionDialog(frame,
-	            message,
-	            "",
-	            JOptionPane.OK_OPTION,
-	            messageType,
-	            null,
-	            options,
-	            options[0]);
-	}
-	
-	// POP UPS end here
-	
-	
-	// CREATING GRAPHICS starts here (here be dragons)
-	
-	// Creates the main window and adds the main panel and the info panel to it
-    private void createFrame() {
-	        frame = new JFrame("Backgammon");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setBounds(100, 100, 600, 400);
-	        frame.setLayout(new GridBagLayout());
-	        frame.setResizable(false);
-	        
-	        GridBagConstraints c = new GridBagConstraints();
-	        c.ipady = 20;
-	        c.gridwidth = GridBagConstraints.REMAINDER;
-	        c.fill = GridBagConstraints.HORIZONTAL;
-	        c.gridx = 0;
-	        c.gridy = 0;
-	        c.weightx = 1.0;
-	        c.weighty = 1.0;
-	        
-	        frame.add(createMainPanel(), c);
-	        
-	        c.insets = new Insets(10,0,0,0);
-	        c.gridx = 0;
-	        c.gridy = 1;
-	        c.weightx = 1.0;
-	        c.weighty = 0.0;
-	        frame.add(createInfoPanel(), c);
-	        frame.setVisible(true);
 
-	        refresh();
-	    }
-	    
+	private boolean showAcceptDoublingPopup() {
+		String message = game.getInActivePlayer().getName()
+				+ ", do you accept doubling the bet?";
+		Object[] options = { "Yes", "No, I'll resign" };
+		int answer = JOptionPane.showOptionDialog(frame, message,
+				"Doubling the bet", JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		if (answer == 0) {
+			game.increaseBet();
+			infoPanel.refresh();
+			return true;
+		} else {
+			game.onWin(game.getActivePlayer());
+			return false;
+		}
+	}
+
+	private void showMessagePopup(String message, String buttonText,
+			int messageType) {
+		Object[] options = { buttonText };
+		JOptionPane.showOptionDialog(frame, message, "", JOptionPane.OK_OPTION,
+				messageType, null, options, options[0]);
+	}
+
+	// POP UPS end here
+
+	// CREATING GRAPHICS starts here (here be dragons)
+
+	// Creates the main window and adds the main panel and the info panel to it
+	private void createFrame() {
+		frame = new JFrame("Backgammon");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 600, 400);
+		frame.setLayout(new GridBagLayout());
+		frame.setResizable(false);
+
+		GridBagConstraints c = new GridBagConstraints();
+		c.ipady = 20;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+
+		frame.add(createMainPanel(), c);
+
+		c.insets = new Insets(10, 0, 0, 0);
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weightx = 1.0;
+		c.weighty = 0.0;
+		frame.add(createInfoPanel(), c);
+		frame.setVisible(true);
+
+		refresh();
+	}
+
 	// Creates the main panel and adds points, bars and homes to it
 	private JPanel createMainPanel() {
 
@@ -237,10 +221,10 @@ public class GFrame {
 		player2Bar = new GBar(this, Player.PLAYER2);
 		player1Home = new GHome(this, Player.PLAYER1);
 		player2Home = new GHome(this, Player.PLAYER2);
-		
+
 		JPanel mainPanel = new JPanel();
 		mainPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		
+
 		mainPanel.setLayout(layout);
 
 		Point[] points = game.getPoints();
@@ -268,18 +252,18 @@ public class GFrame {
 		}
 
 		createGHome(Player.PLAYER1, mainPanel);
-		
+
 		return mainPanel;
 	}
-	
+
 	private void createGPoint(int i, Point[] points, JPanel mainPanel) {
-	        GPoint gPoint = new GPoint(this, points[i]);
-	        JPanel panel = new JPanel();
-	        gPoint.setPanel(panel);
-	        mainPanel.add(panel);
-	        gPoints.add(gPoint);
+		GPoint gPoint = new GPoint(this, points[i]);
+		JPanel panel = new JPanel();
+		gPoint.setPanel(panel);
+		mainPanel.add(panel);
+		gPoints.add(gPoint);
 	}
-	
+
 	private void createGHome(Player player, JPanel mainPanel) {
 		JPanel panel = new JPanel();
 		if (player == Player.PLAYER1) {
@@ -289,7 +273,7 @@ public class GFrame {
 		}
 		mainPanel.add(panel);
 	}
-	
+
 	private void createGBar(Player player, JPanel mainPanel) {
 		JPanel panel = new JPanel();
 		if (player == Player.PLAYER1) {
@@ -299,45 +283,45 @@ public class GFrame {
 		}
 		mainPanel.add(panel);
 	}
-	
-    private JPanel createInfoPanel() {
-      JPanel panel = new JPanel();
-      infoPanel = new GInfo(this);
-      infoPanel.setPanel(panel);
-      return panel;
-    }
+
+	private JPanel createInfoPanel() {
+		JPanel panel = new JPanel();
+		infoPanel = new GInfo(this);
+		infoPanel.setPanel(panel);
+		return panel;
+	}
 
 	// CREATING GRAPHICS ends here
-    
-    // GETTERS FOR INFO PANEL start here
-    
-    public String getDiceInfo() {
-      return getIntegerListAsString(game.getDiceMovesLeft());
-    }
-    
-    public String getPlayerInfo() {
-      return game.getActivePlayer().getName();
-    }
-    
-    public String getBetInfo() {
-      return Integer.toString(game.getBetSize());
-    }
-    
-    public String getAdditionalInfo() {
-      return "TODO";
-    }
-    
-    // GETTERS FOR INFO PANEL start here
-    
-    private String getIntegerListAsString(List<Integer> list) {
-      if (list == null || list.isEmpty()) {
-        return "none";
-      }
-      String str = "";
-      for (Integer i : list) {
-          str += i.toString() + " ";
-      }
-      return str;
-    }
+
+	// GETTERS FOR INFO PANEL start here
+
+	public String getDiceInfo() {
+		return getIntegerListAsString(game.getDiceMovesLeft());
+	}
+
+	public String getPlayerInfo() {
+		return game.getActivePlayer().getName();
+	}
+
+	public String getBetInfo() {
+		return Integer.toString(game.getBetSize());
+	}
+
+	public String getAdditionalInfo() {
+		return "TODO";
+	}
+
+	// GETTERS FOR INFO PANEL start here
+
+	private String getIntegerListAsString(List<Integer> list) {
+		if (list == null || list.isEmpty()) {
+			return "none";
+		}
+		String str = "";
+		for (Integer i : list) {
+			str += i.toString() + " ";
+		}
+		return str;
+	}
 
 }
